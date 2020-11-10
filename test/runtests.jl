1b4b4a767b62f7a8caffa72e73a30edd06880a46
Fons van der Plas <fonsvdplas@gmail.com>
2020-11-10 19:15:15 +0100

    hello pluto

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.12.7
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 52305f60-2380-11eb-0a3b-6f693199061f
 using Test
 
-@test false
+# ╔═╡ 548069e0-2380-11eb-0515-49f9a0b7a819
+x = true
+
+# ╔═╡ 49f1edf0-2380-11eb-3d9e-2bcdf561c818
+@test x == false
+
+# ╔═╡ Cell order:
+# ╠═49f1edf0-2380-11eb-3d9e-2bcdf561c818
+# ╠═52305f60-2380-11eb-0a3b-6f693199061f
+# ╠═548069e0-2380-11eb-0515-49f9a0b7a819
